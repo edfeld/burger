c5a089e05d454e9613d66c65256100e3874c494a
edfeld <edfeld1@gmail.com>
2018-11-12 23:29:54 -0800

    have a working prototype

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ("Richards Deluxe");
+
+INSERT INTO burgers (burger_name) VALUES ("Taco Tavern Double");
+
+INSERT INTO burgers (burger_name) VALUES ("bonzai burger");
